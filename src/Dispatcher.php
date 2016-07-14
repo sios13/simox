46454c67e60de7cf0a82db028935f43f51b9437f
Simon <simon2@live.se>
2016-07-14 14:35:59 +0200

    Add listeners and error handling to the Dispatcher
    The dispatcher will now throw exceptions when there is cyclic routing and
    when a controller or action is not found.
    
    Added listener "beforeExecuteRoute" and "beforeException".

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -3,42 +3,58 @@ namespace Simox;
 
 class Dispatcher extends SimoxServiceBase implements Events\EventsAwareInterface
 {
-    private $controller;
-    private $action;
-    private $params;
-    
-    private $controller_name;
-    private $action_name;
+    private $_controller_name;
+    private $_action_name;
+    private $_params;
 	
-	private $events_manager;
+	private $_events_manager;
 	
-	private $was_forwarded;
+	private $_was_forwarded;
     
-    private $no_match_path;
+    const EXCEPTION_CYCLIC_ROUTING = 0;
+    
+    const EXCEPTION_CONTROLLER_NOT_FOUND = 1;
+    
+    const EXCEPTION_ACTION_NOT_FOUND = 2;
 
     public function __construct()
     {
+        $this->_params = array();
+        
 		$this->was_forwarded = false;
     }
     
 	public function setEventsManager( Events\Manager $events_manager )
 	{
-		$this->events_manager = $events_manager;
+		$this->_events_manager = $events_manager;
 	}
 	
 	public function getEventsManager()
 	{
-		return $this->events_manager;
+		return $this->_events_manager;
 	}
+    
+    public function setControllerName( $controller_name )
+    {
+        $this->_controller_name = $controller_name;
+    }
+    
+    public function setActionName( $action_name )
+    {
+        $this->_action_name = $action_name;
+    }
+    
+    public function setParams( $params )
+    {
+        $this->_params = $params;
+    }
 	
     /**
      * @return Returns the name of the controller being dispatched
      */
 	public function getControllerName()
 	{
-        $this->controller_name = strtolower( $this->controller_name );
-        $this->controller_name = str_replace( "controller", "", $this->controller_name );
-        return $this->controller_name;
+        return $this->_controller_name;
 	}
 	
     /**
@@ -46,10 +62,37 @@ class Dispatcher extends SimoxServiceBase implements Events\EventsAwareInterface
      */
 	public function getActionName()
 	{
-        $this->action_name = strtolower( $this->action_name );
-        $this->action_name = str_replace( "action", "", $this->action_name );
-        return $this->action_name;
+        return $this->_action_name;
 	}
+    
+    /**
+     * Handles the exceptions thrown in the Dispatcher.
+     * The exception can be handled by the "beforeException" listener.
+     * If the listener returns false, _throwDispatchException also returns false.
+     * The header status code is always set to 404.
+     */
+    private function _throwDispatchException( $message, $exception_code = 0 )
+    {
+        /**
+         * Sets a 404 header
+         */
+        $this->response->setStatusCode( 404, "Not found" );
+        
+        $exception = new \Exception( $message, $exception_code );
+        
+		if ( is_object($this->_events_manager) )
+        {
+            /**
+             * If the event returns false, exit early and don't throw exception
+             */
+            if ( $this->_events_manager->fire( "dispatch:beforeException", $this, array("exception" => $exception) ) == false )
+            {
+                return false;
+            }
+		}
+        
+        throw $exception;
+    }
 	
     /**
      * Tells the dispatcher to forward to another controller and action
@@ -58,111 +101,94 @@ class Dispatcher extends SimoxServiceBase implements Events\EventsAwareInterface
      * @param string $params["action"] name of the action
      * @param array $params["params"] parameters to the action
      */
-	public function forward( $_params )
+	public function forward( $params )
 	{
-		$controller = ucfirst( strtolower( $_params["controller"] ) ) . "Controller";
-		$action = strtolower( $_params["action"] ) . "Action";
-        $params = isset($_params["params"]) ? $_params["params"] : array();
+        $this->_controller_name = $params["controller"];
+        $this->_action_name     = $params["action"];
+        $this->_params          = isset($params["params"]) ? $params["params"] : array();
         
-		$this->controller = new $controller();
-		$this->action = $action;
-        $this->params = $params;
-		
-		$this->was_forwarded = true;
+		$this->_was_forwarded = true;
 	}
     
     /**
-     * Sets the $no_match_path
-     *
-     * If the router tells the dispatcher that there is no match,
-     * the $no_match_path can tell the dispatcher what controller#action to call
-     * 
-     * @param array $target ["controller" => "index", "action" => "index"]
+     * Dispatch to a controller/action provided by the router
      */
-    public function setNoMatchPath( $target )
+    public function dispatch()
     {
-        $this->no_match_path = array( "target" => $target, "params" => array() );
-    }
-    
-    /**
-     * Private helper function to explode a target (Controller#action) into array
-     * 
-     * @param string $target
-     * @return array
-     */
-    private function _explodeTarget( $target )
-    {
-        $target = explode( "#", $target );
-        
-        return array(
-            "controller_name" => $target[0],
-            "action_name" => $target[1]
-        );
-    }
-    
-    /**
-     * Dispatch the flow to a controller/action provided by the router
-     */
-    public function dispatch( $match )
-    {
-        // If there was no match
-        if ( $match == false )
+		/**
+         * Calling before dispatch listener
+         */
+		if ( is_object($this->_events_manager) )
         {
-            // 404 header
-            header("HTTP/1.0 404 Not Found");
-            
-            if ( isset($this->no_match_path) )
-            {
-                $match = $this->no_match_path;
-            }
-            else
-            {
-                throw new \Exception("404. Page not found.");
-            }
-        }
-        
-        // Controller#action
-        $target = $this->_explodeTarget( $match["target"] );
-        
-        $this->controller_name = $target["controller_name"];
-        $this->action_name = $target["action_name"];
-        
-        $this->controller = new $target["controller_name"]();
-        $this->action = $target["action_name"];
-        $this->params = $match["params"];
-        
-        
-		// Calling before dispatch listener
-		if ($this->events_manager) {
-			$this->events_manager->fire( "dispatch:beforeDispatch", $this );
-			
-			// If there was a forward in the listener
-			if ($this->was_forwarded == true) {
-				$this->was_forwarded = false;
-				//continue;
-			}
+            $this->_events_manager->fire( "dispatch:beforeDispatch", $this );
 		}
 		
 		$finished = false;
 		
 		$number_dispatches = 0;
 		
-		while (!$finished)
+		while ( !$finished )
 		{
 			$number_dispatches++;
 			
-			if ($number_dispatches == 100) {
-				throw new \Exception("Dispatch error. Cyclic routing.");
+			if ($number_dispatches == 100)
+            {
+                $this->_throwDispatchException( "Dispatch error. Cyclic routing.", self::EXCEPTION_CYCLIC_ROUTING );
 				break;
 			}
+            
+            $controller_class = ucfirst($this->_controller_name) . "Controller";
+            
+            if ( !class_exists($controller_class) )
+            {
+                if ( $this->_throwDispatchException( $this->_controller_name . " controller class cannot be loaded.", self::EXCEPTION_CONTROLLER_NOT_FOUND ) == false )
+                {
+                    continue;
+                }
+                break;
+            }
+            
+            $controller = new $controller_class();
+            
+            if ( !method_exists($controller, $this->_action_name . "Action") )
+            {
+                if ( $this->_throwDispatchException( $this->_action_name . "Action action does not exist in " . $controller_class . ".", self::EXCEPTION_ACTION_NOT_FOUND ) == false )
+                {
+                    continue;
+                }
+                break;
+            }
+            
+            /**
+             * Calling before execute route listener
+             */
+            if ( is_object($this->_events_manager) )
+            {
+                $this->_events_manager->fire( "dispatch:beforeExecuteRoute", $this );
+                
+                /**
+                 * If there was a forward in the listener
+                 */
+                if ( $this->_was_forwarded )
+                {
+                    $this->_was_forwarded = false;
+                    continue;
+                }
+            }
+            
+            $controller->initialize();
+            
+			/**
+             * Calling action in controller with params
+             */
+			call_user_func_array( array($controller, $this->_action_name . "Action"), $this->_params );
 			
-			// Calling action in controller with params
-            $this->controller->initialize();
-			call_user_func_array( array($this->controller, $this->action), $this->params );
-			
-			// If there was a forward in the action
-			if ($this->was_forwarded == true) {
-				$this->was_forwarded = false;
+			/**
+             * If there was a forward in the action
+             */
+			if ($this->_was_forwarded == true)
+            {
+				$this->_was_forwarded = false;
 				continue;
 			}
 			
