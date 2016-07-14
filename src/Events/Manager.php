7111d7a2f50e68729e4a2b85a63f1b6f3674f341
Simon <simon2@live.se>
2016-07-14 14:42:58 +0200

    Give closure/callable support for the events manager
    You can now add events as closures/callables, instead of only plugin
    classses.

--- src/Events/Manager.php
+++ src/Events/Manager.php
@@ -3,32 +3,38 @@ namespace Simox\Events;
 
 class Manager
 {
-	private $attached_events;
+	private $_events;
 	
 	public function __construct()
 	{
-		$this->attached_events = array();
+		$this->_events = array();
 	}
     
     public function _explodeEventName( $event_name )
     {
         $target = explode( ":", $event_name );
+        
         return array( "component_name" => $target[0], "task_name" => $target[1] );
     }
 	
 	public function attach( $event_name, $plugin )
 	{
-		$this->attached_events[$event_name] = $plugin;
+		$this->_events[$event_name] = $plugin;
 	}
 	
-	public function fire( $event_name, $component )
+	public function fire( $event_name, $component, $params = null )
 	{
-        if ( isset($this->attached_events[$event_name]) )
+        if ( isset($this->_events[$event_name]) )
         {
             $target = $this->_explodeEventName( $event_name );
-            if ( method_exists($this->attached_events[$event_name], $target["task_name"]) )
+            
+            if ( method_exists($this->_events[$event_name], $target["task_name"]) )
             {
-                $this->attached_events[$event_name]->$target["task_name"]( $component );
+                return $this->_events[$event_name]->$target["task_name"]($component, $params);
+            }
+            else if ( is_callable($this->_events[$event_name]) )
+            {
+                return call_user_func_array( $this->_events[$event_name], array($component, $params) );
             }
         }
 	}
