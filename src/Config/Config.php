6f02884d8d6db46b70c1fedd26b3d9117d9c476d
Simon <simon2@live.se>
2016-07-19 08:21:33 +0200

    Add first iteration of config

---
+++ src/Config/Config.php
@@ -0,0 +1,40 @@
+<?php
+namespace Simox;
+
+class Config extends SimoxServiceBase implements \ArrayAccess
+{
+	public function __construct( $config_array = array() )
+    {
+        foreach ( $config_array as $key => $value )
+        {
+            $this->offsetSet( $key, $value );
+        }
+    }
+    
+    public function offsetExists( $key )
+    {
+        return isset($this->$key);
+    }
+    
+    public function offsetGet( $key )
+    {
+        return $this->$key;
+    }
+    
+    public function offsetSet( $key, $value )
+    {
+        if ( is_array($value) )
+        {
+            $this->$key = new self($value);
+        }
+        else
+        {
+            $this->$key = $value;
+        }
+    }
+    
+    public function offsetUnset( $key )
+    {
+        $this->$key = null;
+    }
+}
