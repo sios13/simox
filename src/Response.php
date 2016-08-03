8f1acc5f3fc2ac9b59ef551000c972d84dc43ba0
Simon <simon2@live.se>
2016-08-03 11:31:29 +0200

    add a redirect function to response

--- src/Response.php
+++ src/Response.php
@@ -24,13 +24,23 @@ class Response extends SimoxServiceBase
         $this->_headers[] = array("code" => $code, "message" => $message);
     }
     
+    public function redirect( $params )
+    {
+        if ( is_array($params) )
+        {
+            $uri = $this->router->reverseRoute( $params["controller"], $params["action"] );
+        }
+        
+        $this->setStatusCode( 302, "Location: http://" . $this->request->getServer("HTTP_HOST") . $this->url->get($uri) );
+    }
+    
     public function sendHeaders()
     {
         if ( is_array($this->_headers) )
         {
             foreach ( $this->_headers as $header )
             {
-                header( "HTTP/1.1 " . $header["code"] . " " . $header["message"], true, $header["code"] );
+                header( $header["message"], true, $header["code"] );
             }
         }
     }
