a23d950aade83d8d135e27921162d07054e4a83c
Simon <simon2@live.se>
2016-08-03 11:29:25 +0200

    remove redirect from controller, add to response instead

--- src/Controller.php
+++ src/Controller.php
@@ -6,23 +6,5 @@ abstract class Controller extends SimoxServiceBase
 {
     public function __construct() {}
     
-    public function initialize(){}
-    
-    public function redirect( $path, $is_local_path = true )
-    {
-        if ( $is_local_path )
-        {
-            $full_path = $this->url->get( $path );
-            
-            // Remove excess slash
-            $full_path = preg_replace('#/+#','/', $full_path);
-        }
-        else
-        {
-            $full_path = $path;
-        }
-        
-        header( "Location: $full_path" );
-        die();
-    }
+    public function initialize() {}
 }
