231222ae78fe5d193a51923cea52f1acb111f993
Simon <simon2@live.se>
2016-08-17 11:56:45 +0200

    make the controller DI aware

--- src/Controller.php
+++ src/Controller.php
@@ -1,10 +1,36 @@
 <?php
-
 namespace Simox;
 
-abstract class Controller extends SimoxServiceBase
+use Simox\DI\DIAwareInterface;
+
+abstract class Controller implements DIAwareInterface
 {
+	private $_di;
+
     public function __construct() {}
+
+    public function setDI( $di )
+    {
+    	$this->_di = $di;
+    }
+
+    public function getDI()
+    {
+    	return $this->_di;
+    }
+
+    public function __get( $var_name )
+    {
+        if ( $this->_di->hasService($var_name) )
+        {
+            return $this->_di->getService( $var_name );
+        }
+
+        if ( $var_name == "di" )
+        {
+            return $this->_di;
+        }
+    }
     
     public function initialize() {}
 }
