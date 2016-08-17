bc57c36772719336c2613e6e49fb68a412bfa850
Simon <simon2@live.se>
2016-08-17 11:54:53 +0200

    make plugins get their dependencies from the DI class statically

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,7 +1,22 @@
 <?php
 namespace Simox;
 
-abstract class Plugin extends SimoxServiceBase
+abstract class Plugin
 {
 	public function __construct() {}
+
+	public function __get( $var_name )
+	{
+		$di = DI::getDefault();
+
+        if ( $di->hasService($var_name) )
+        {
+            return $di->getService( $var_name );
+        }
+
+        if ( $var_name == "di" )
+        {
+            return $di;
+        }
+	}
 }
