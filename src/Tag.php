a7b51a38363785c5707691b6e86c4597a432e2cb
Simon Österdahl <so222et@student.lnu.se>
2016-08-18 13:10:30 +0200

    update tag to use the DI

--- src/Tag.php
+++ src/Tag.php
@@ -1,15 +1,40 @@
 <?php
 namespace Simox;
 
-class Tag extends SimoxServiceBase
+use Simox\DI\DIAwareInterface;
+
+class Tag implements DIAwareInterface
 {
-	public function __construct() {}
+    private $_di;
+
+    private $_title;
+
+    private $_title_append;
+    
+    private $_title_prepend;
+
+	public function __construct()
+    {
+        $this->_di = null;
+
+        $this->_title = null;
+
+        $this->_title_append = null;
+
+        $this->_title_prepend = null;
+    }
+
+    public function setDI( $di )
+    {
+        $this->_di = $di;
+    }
+
+    public function getDI()
+    {
+        return $this->_di;
+    }
     
     /* ===== TITLE ===== */
-    
-    private $_title;
-    private $_title_append;
-    private $_title_prepend;
 
     /**
      * Sets a title
@@ -62,9 +87,11 @@ class Tag extends SimoxServiceBase
      */
     public function linkTo( $path, $text, $is_local_path = true )
     {
+        $url = $this->_di->getService( "url" );
+        
         if ( $is_local_path )
         {
-            $path = $this->url->get( $path );
+            $path = $url->get( $path );
         }
 		
         $link = "<a href='$path'>$text</a>";
@@ -73,6 +100,7 @@ class Tag extends SimoxServiceBase
     }
 	
 	/* ==== IMAGES ==== */
+
 	/**
      * @param string $path path to the image relative to the /public/ folder
      * @param string $alt alternative text to the img-tag
@@ -81,7 +109,9 @@ class Tag extends SimoxServiceBase
      */
 	public function image( $path, $alt, $options = array() )
 	{
-        $path = $this->url->get( $path );
+        $url = $this->_di->getService( "url" );
+
+        $path = $url->get( $path );
         
 		$image_tag = "<img src='". $path ."'";
         $image_tag .= " alt='". $alt ."'";
@@ -102,6 +132,7 @@ class Tag extends SimoxServiceBase
 	}
     
     /* ===== STYLESHEET ===== */
+
 	/**
      * @param string $path path to the stylesheet realtive to the /public/ folder
      * @param boolean $local_path
@@ -109,9 +140,11 @@ class Tag extends SimoxServiceBase
      */
 	public function stylesheetLink( $path, $is_local_path = true )
 	{
+        $url = $this->_di->getService( "url" );
+
         if ( $is_local_path )
         {
-            $path = $this->url->get( $path );
+            $path = $url->get( $path );
         }
         
         $stylesheet_tag = "<link href='" . $path . "' rel='stylesheet' type='text/css'>";
@@ -120,6 +153,7 @@ class Tag extends SimoxServiceBase
 	}
     
     /* ===== JAVASCRIPT ===== */
+    
     /**
      * @param string $path path to the javascript file relative to the /public/ folder
      * @param boolean $local_path
@@ -127,9 +161,11 @@ class Tag extends SimoxServiceBase
      */
     public function javascriptInclude( $path, $is_local_path = true )
     {
+        $url = $this->_di->getService( "url" );
+        
         if ( $is_local_path )
         {
-            $path = $this->url->get( $path );
+            $path = $url->get( $path );
         }
         
         $javascript_tag = "<script src='" . $path . "'></script>";
